6c7bfc85c48991ef0c9313082f9591d249849325
Bang Vo <bavo.imp@gmail.com>
2022-11-09 23:25:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 
-from model import get_prediction
+# from model import get_prediction
 
 def url_to_img(url, save=True):
   img = Image.open(BytesIO(requests.get(url).content))
@@ -19,17 +19,17 @@ app = Flask(__name__)
 def greeting():
     return "Skin detective API."
 
-@app.route('/predict', methods=["POST"])
-def predict():
-    data = request.get_json()
-    url = data.get('image_url')
-    if url:
-        pil_img = url_to_img(url)
+# @app.route('/predict', methods=["POST"])
+# def predict():
+#     data = request.get_json()
+#     url = data.get('image_url')
+#     if url:
+#         pil_img = url_to_img(url)
 
-    result = get_prediction(pil_img)
-    print(result)
+#     result = get_prediction(pil_img)
+#     print(result)
     
-    return result
+#     return result
 
 
 @app.route('/healthcheck')
