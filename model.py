f5caa7a142598b2128e457e2e21309e14f3d7569
Bang Vo <bavo.imp@gmail.com>
2022-12-04 18:55:27 +0700

    minor

--- model.py
+++ model.py
@@ -29,7 +29,7 @@ def get_prediction(image, thres=0):
         [
           transforms.Resize(size=img_size),
           transforms.ToTensor(),
-          # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
+          transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
           ])
     batch_size = 1
     trans_img = transform(image)
